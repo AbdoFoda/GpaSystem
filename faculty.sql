a9412c2ae092328ac3f498ed640065bb38779eaf
Abdo <Abdo@MacBook-Pro.local>
2018-11-21 22:45:14 +0200

    add database script

---
+++ faculty.sql
@@ -0,0 +1,10 @@
+create database faculty;
+
+create table student(
+	id varchar(50),
+    sname varchar(50),
+    address varchar(50),
+    gpa float
+);
+
+insert INTO student values(20150151,'abdo','haram',3.00);
